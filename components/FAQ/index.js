e061a4abc336a0e26b48c225ca3e91b2f6298a4e
Brian Farmer <brianlfarmerllc@gmail.com>
2021-09-26 21:44:24 -0500

    finished FAQ component

--- components/FAQ/index.js
+++ components/FAQ/index.js
@@ -1,57 +1,51 @@
+import { useState } from "react";
+import Image from "next/image";
 import GreenButton from "../GreenButton";
+import faq from "../../db/FAQ.json";
+import arrow from "../../public/assets/Icons/arrow.png";
 
 export default function FAQ() {
+  const [currentQuestion, setCurrentQuestion] = useState("");
+
+  function rotateArrow(e) {
+    // get active elements
+    const activeQuestion = e.target.name;
+
+    // check to see if active is current if not set active to current
+    currentQuestion === activeQuestion
+      ? setCurrentQuestion("")
+      : setCurrentQuestion(activeQuestion);
+  }
   return (
     <section className="white-section">
       <div className="faq">
         <h2 className="faq-header">Frequently Asked Questions</h2>
         <div className="questions">
-          <div className="qa">
-            <h3 className="question">Am I the right trainer for you?</h3>
-            <h4 className="answer">
-              It is important to understand that having a well trained dog will likely require some
-              lifestyle changes. I am ready to do whatever it takes for you and your dog to be
-              successful, as long as I know you are ready to make the same commitment to me and your
-              dog. I will reward your dog, but I will also correct unwanted behavior.{" "}
-            </h4>
-          </div>
-          <div className="qa">
-            <h3 className="question">Are you the right client for me?</h3>
-            <h4 className="answer">
-              I am looking for clients who are going to follow through on their homework. Your
-              follow through at home is imperative to the success of your dog's training. I will
-              assign homework for you to complete while your dog is in training. Completing this
-              homework is essential to long term success and maintaining the training they receive.
-            </h4>
-          </div>
-          <div className="qa">
-            <h3 className="question">What tools do you use?</h3>
-            <h4 className="answer">
-              It is important to have a large "tool box" when it comes to training dogs. There is no
-              one-size-fits-all approach to dog training. Some tools I use include: prong collars,
-              e-collars, slip leads, Pet Convincer (compressed air for interrupting unwanted
-              behavior), my dog, long-lines and food. The goal of this training is to achieve the
-              result of a calm, relaxed, tuned-in, confident, happy and obedient companion.
-            </h4>
-          </div>
-          <div className="qa">
-            <h3 className="question">Can you tell me more about prong collars?</h3>
-            <h4 className="answer">
-              Prong collars are the least physically taxing tool available to both you and your dog.
-              It allows you to use very little physical effort to communicate with your dog. Many
-              tools only mask bad behaviors, but prong collars allow us to create a different state
-              of mind for your dog.
-            </h4>
-          </div>
-          <div className="qa">
-            <h3 className="question">Can you tell me more about e-collars?</h3>
-            <h4 className="answer">
-              E-collars come with a remote and are a fantastic way to communicate with your dog. It
-              can provide off-leash freedom for your dog in certain scenarios. We use a top of the
-              line remote e-collar system with 100 levels. This allows us to fine tune our training,
-              so each dog is using the lowest level possible for communication.
-            </h4>
-          </div>
+          {faq.map((qa, index) => (
+            <div key={index} className="qa">
+              <div className="question-container">
+                <h3
+                  name={index}
+                  style={{ color: currentQuestion == index ? "#2abaa0" : "#383838" }}
+                  className="question"
+                >
+                  {qa.question}
+                </h3>
+                <Image
+                  name={index}
+                  className={currentQuestion == index ? "arrow rotate-arrow" : "arrow"}
+                  onClick={rotateArrow}
+                  src={arrow}
+                />
+              </div>
+              <h4
+                name={index}
+                className={currentQuestion == index ? "answer show-answer" : "answer"}
+              >
+                {qa.answer}
+              </h4>
+            </div>
+          ))}
         </div>
       </div>
     </section>
